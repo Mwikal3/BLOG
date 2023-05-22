cba7a3c4ceae892e8dd31ffe23005ab09d1ad7de
mwikal3 <mwikalifay14@gmail.com>
2023-05-22 12:08:02 +0300

    new blog

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -14,7 +14,10 @@
         <h1>My second post</h1>
         <p>One of the misconceptions about starting a blog is that you need to be a great writer to be successful. Nothing could be further from the truth. People read blog sites to get a personal perspective on things, so most bloggers write in a very informal and conversational style.</p>
 </article>
+
+<article>
+        <h1>My second post</h1>
+        <p>One of the misconceptions about starting a blog is that you need to be a great writer to be successful. Nothing could be further from the truth. People read blog sites to get a personal perspective on things, so most bloggers write in a very informal and conversational style.</p>
+</article>
 </body>
-
-
 </html>
